59310644f83f7d627ea376a47c3e1c3745e0c7b2
grovy73 <holger.bregendahl@hotmail.com>
2026-02-02 16:44:23 +0100

    Tried forking and execvp, but I'll have to keep experimenting

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
   
@@ -11,6 +13,19 @@ int main(int argc, char *argv[]) {
     fgets(inBuffer, 64, stdin);
 
     if(!strncmp(inBuffer, "exit\n", 64)) break;
+
+    pid_t childPid = fork();
+
+    if(!childPid) { // Child
+      execvp("asdha", NULL);
+      perror("execvp");
+      break;
+    } else if (pid >  0) { // Parent
+      wait(NULL);
+    } else {
+      perror("fork");
+    }
+
   }
 
   return 0;
