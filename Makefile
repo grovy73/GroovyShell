f4966ada16b5d529f249c4bb5e5c84401dfd6c7c
grovy73 <holger.bregendahl@hotmail.com>
2026-02-07 18:04:22 +0100

    Implemented parseArgs function

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
+FLAGS = -Iincludes
+
 build:
-	gcc src/*.c -o gsh -Iincludes
+	gcc src/*.c -o gsh $(FLAGS)
+debug:
+	gcc src/*.c -o gsh_debug $(FLAGS) -g
 run:
 	./gsh
 clean:
-	rm gsh
+	rm gsh gsh_debug
--- src/main.c
+++ src/main.c
@@ -3,8 +3,33 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+int countWords(char* src) {
+  int count = 1;
+  int shouldCountSpace;
+  int haveEncounteredLetter = 0;
+  for(int i = 0; src[i]; i++) {
+    if(!haveEncounteredLetter && src[i] > 32 && src[i] < 127) {haveEncounteredLetter = 1;}
+    shouldCountSpace = src[i] == ' ' && i != 0 && src[i + 1] != ' ' && src[i + 1] != '\0' && haveEncounteredLetter;
+
+    if(shouldCountSpace) {
+      count++;
+    }
+  }
+  if(!haveEncounteredLetter) {count = 0;}
+  return count;
+}
+
+void parseArgs(char* src, char** dest) {
+  char* word = strtok(src, " ");
+
+  int i = 0;
+  while(word) {
+    dest[i++] = word;
+    word = strtok(NULL, " ");
+  }
+}
+
 int main(int argc, char *argv[]) {
-  
   char inBuffer[64];
   // REPL Loop
   for (;;) {
@@ -13,19 +38,10 @@ int main(int argc, char *argv[]) {
     fgets(inBuffer, 64, stdin);
 
     if(!strncmp(inBuffer, "exit\n", 64)) break;
-
-    pid_t childPid = fork();
-
-    if(!childPid) { // Child
-      execvp("asdha", NULL);
-      perror("execvp");
-      break;
-    } else if (pid >  0) { // Parent
-      wait(NULL);
-    } else {
-      perror("fork");
-    }
-
+    
+    int argsSize = countWords(inBuffer);
+    char* args[argsSize];
+    parseArgs(inBuffer, args);
   }
 
   return 0;
